1459f591a0e6a3aa021b831c0f88644a786d508f
Shreya <shreyareddy1812002.com>
2022-11-23 12:33:06 +0530

    lab internal

---
+++ src/main/java/pom/maven/SampleMavenExam/App.java
@@ -0,0 +1,13 @@
+package pom.maven.SampleMavenExam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "this is se lab internal" );
+    }
+}
